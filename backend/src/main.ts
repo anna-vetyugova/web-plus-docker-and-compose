adf50b53bd24aa67ec97affd80d33e670428d5c5
Anna Vetyugova <anna.vetyugova@sabris.ru>
2024-08-08 12:26:15 +0300

    fix: add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
