e4770b1145c8b4db54d3d6252b49477157366a10
Anna Vetyugova <anna.vetyugova@sabris.ru>
2024-08-08 12:56:59 +0300

    fix: fix some errors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors();
+  // Настройка CORS
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, 
+  });
+
+  // app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
