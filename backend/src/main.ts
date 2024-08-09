1f930cd67227f7604103ee97d307a29aa7815744
Anna Vetyugova <anna.vetyugova@sabris.ru>
2024-08-09 16:19:57 +0300

    fix: nginx conf

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,13 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule, { cors: true });
-  // Настройка CORS
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, 
-  });
-
+  app.enableCors();
   // app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
